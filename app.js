08bdf63a63fe0f2738d3d8a4a0a132fd0835db54
Waqarghori <waqar.iqbal789@gmail.com>
2025-01-10 00:06:06 +0500

    update

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-// function bloodGrp (grp){
-//     let document.getElementById("chekBtn") ;
-//     if ( grp === "o+"){
-//         console.log("O+ & O-")
-//     }
-// }
+
 
 function bloodGrp(grp) {
-    // Find the submit button by ID
-    let user = document.getElementById("chekBtn");
-
-    // Check the blood group value
-    if (grp === "o+") {
-        console.log("Compatible donors: O+ & O-");
-    } else {
-        console.log("No match found for the given group.");
+    let output = "";
+    if (grp === "O+"){
+        output = "Compatible donors: O+ & O+";
+    }else if (grp === "O-"){
+        output = "Compatible donors: Only O-";
     }
+
+    document.getElementById("output").innerText = output;
 }
+
+

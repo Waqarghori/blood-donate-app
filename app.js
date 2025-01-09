f281276877593d6f1627e7f19579aa8e7a832f60
Waqarghori <waqar.iqbal789@gmail.com>
2025-01-10 00:51:20 +0500

    Updated

--- app.js
+++ app.js
@@ -2,22 +2,22 @@
 
 function bloodGrp(grp) {
     let output = "";
-    if (grp === "O+") {
-        output = "Compatible donors: O+ & O+";
-    } else if (grp === "O-") {
-        output = "Compatible donors: Only O-";
-    } else if (grp === "A+") {
-        output = "Compatible donors: A+, A-, O+ & O-";
-    } else if (grp === "A-") {
-        output = "Compatible donors: A- & O-";
-    } else if (grp === "B+") {
-        output = "Compatible donors: B+, B-, O+ & O-";
-    } else if (grp === "B-") {
-        output = "Compatible donors: O- & B-"
-    } else if (grp === "AB+") {
-        output = "Compatible donors: O-, O+, A-, A+";
-    } else if (grp === "AB-") {
-        output = "Compatible donors: O-, A-, B-, AB-";
+    if (grp === "O+"){
+        output = "Compatible donors for O+: (O+) & (O-)";
+    }else if (grp === "O-"){
+        output = "Compatible donors for O-: Only (O-)";
+    }else if(grp === "A+"){
+        output = "Compatible donors for A+: (A-Positiv), (A-Nagativ), (O-Positive) & (O-Nagativ)";
+    }else if (grp === "A-"){
+        output = "Compatible donors for A-: (A-) & (O-)";
+    }else if (grp === "B+"){
+        output = "Compatible donors for B+: (B+), (B-), (O+) & (O-)";
+    }else if (grp === "B-"){
+        output = "Compatible donors for B-: (O-) & (B-)"
+    }else if (grp === "AB+"){
+        output = "Compatible donors for AB+: (O-), (O+), (A-), (A+)";
+    }else if(grp === "AB-"){
+        output = "Compatible donors for AB-: (O-), (B-), (A-) & (AB-)";
     }
     document.getElementById("output").innerText = output;
 }

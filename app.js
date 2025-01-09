0023aea9bd02c53ff5aa9154fe52b06b51f4eabb
Waqarghori <waqar.iqbal789@gmail.com>
2025-01-10 00:53:46 +0500

    Update

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ function bloodGrp(grp) {
     }else if (grp === "O-"){
         output = "Compatible donors for O-: Only (O-)";
     }else if(grp === "A+"){
-        output = "Compatible donors for A+: (A-Positiv), (A-Nagativ), (O-Positive) & (O-Nagativ)";
+        output = "Compatible donors for A+: (A+) , (A-) , (O+) & (O-)";
     }else if (grp === "A-"){
         output = "Compatible donors for A-: (A-) & (O-)";
     }else if (grp === "B+"){
-        output = "Compatible donors for B+: (B+), (B-), (O+) & (O-)";
+        output = "Compatible donors for B+: (B+) , (B-) , (O+) & (O-)";
     }else if (grp === "B-"){
         output = "Compatible donors for B-: (O-) & (B-)"
     }else if (grp === "AB+"){
-        output = "Compatible donors for AB+: (O-), (O+), (A-), (A+)";
+        output = "Compatible donors for AB+: (O-) , (O+) , (A-) , (A+)";
     }else if(grp === "AB-"){
-        output = "Compatible donors for AB-: (O-), (B-), (A-) & (AB-)";
+        output = "Compatible donors for AB-: (O-) , (B-) , (A-) & (AB-)";
     }
     document.getElementById("output").innerText = output;
 }

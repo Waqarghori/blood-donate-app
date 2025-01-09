65d7cf8b4f9d8dc6d64571678f1cae5ba500297d
Waqarghori <waqar.iqbal789@gmail.com>
2025-01-10 00:24:32 +0500

    update

--- app.js
+++ app.js
@@ -2,13 +2,22 @@
 
 function bloodGrp(grp) {
     let output = "";
-    if (grp === "O+"){
+    if (grp === "O+") {
         output = "Compatible donors: O+ & O+";
-    }else if (grp === "O-"){
+    } else if (grp === "O-") {
         output = "Compatible donors: Only O-";
+    } else if (grp === "A+") {
+        output = "Compatible donors: A+, A-, O+ & O-";
+    } else if (grp === "A-") {
+        output = "Compatible donors: A- & O-";
+    } else if (grp === "B+") {
+        output = "Compatible donors: B+, B-, O+ & O-";
+    } else if (grp === "B-") {
+        output = "Compatible donors: O- & B-"
+    } else if (grp === "AB+") {
+        output = "Compatible donors: O-, O+, A-, A+";
+    } else if (grp === "AB-") {
+        output = "Compatible donors: O-, A-, B-, AB-";
     }
-
     document.getElementById("output").innerText = output;
 }
-
-
